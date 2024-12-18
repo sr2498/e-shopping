04f1a3117404e31e33960a0d8a1b345ae5e2ad87
Sandhya Rani <sr2498@njit.edu>
2024-12-17 23:59:48 -0500

    Initial commit with Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+        </a>
+        <a href="https://react.dev" target="_blank">
+        </a>
+      </div>
+      <h1>e-shopping</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
